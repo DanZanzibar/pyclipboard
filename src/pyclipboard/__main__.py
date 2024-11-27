c1625e11df922cb881c808d99ae457388da700ea
Zan Owsley <zanowsley@gmail.com>
2024-11-27 16:05:09 -0700

    Reworked pyclipboard to use dmenu

--- src/pyclipboard/__main__.py
+++ src/pyclipboard/__main__.py
@@ -1,28 +1,8 @@
-import readline
-import re
-import os.path
 import pyperclip
-from pyclipboard.pyclipboard import complete, SNIPPET_DIR
+from pyclipboard.pyclipboard import SNIPPET_DIR, choose_snippet, replace_placeholders
 
 
-readline.parse_and_bind('tab: complete')
-readline.set_completer(complete)
-
-snippet_name = input('Which snippet?\n> ')
-snippet_path = os.path.join(SNIPPET_DIR, (snippet_name + '.txt'))
-
-with open(snippet_path, 'r') as f:
-    snippet = f.read()
-
-readline.set_completer()
-
-placeholders = re.findall(r'~!~[^(~!~)]*~!~', snippet)
-finished_placeholders = []
-
-for placeholder in placeholders:
-    if placeholder not in finished_placeholders:
-        new_text = input(placeholder[3: -3] + ': ')
-        snippet = snippet.replace(placeholder, new_text)
-        finished_placeholders.append(placeholder)
+snippet = choose_snippet(SNIPPET_DIR)
+snippet = replace_placeholders(snippet)
 
 pyperclip.copy(snippet)
--- src/pyclipboard/pyclipboard.py
+++ src/pyclipboard/pyclipboard.py
@@ -1,22 +1,50 @@
 import os
+import re
+from subprocess import run, PIPE, DEVNULL
 
 
-SNIPPET_DIR = "/home/zan/sync-general/bin/snippets/"
+SNIPPET_DIR = "/home/zan/sync/dat/snippets/"
 
+def dmenu_input(prompt: str) -> str:
+    process = run(
+        ['dmenu', '-p', prompt],
+        stdin=DEVNULL,
+        stdout=PIPE,
+        text=True)
 
-snippets = [file_name.split('.')[0]
-            for file_name in os.listdir(SNIPPET_DIR)]
+    return process.stdout.strip()
 
+def snippet_names(snippet_dir: str) -> list[str]:
+    return [file_name.split('.')[0]
+            for file_name in os.listdir(snippet_dir)]
+    
+def choose_snippet(snippet_dir: str) -> str:
+    snippets = snippet_names(snippet_dir)
+    snippets = '\n'.join(snippets)
+    process = run(
+        ['dmenu', '-p', 'Which snippet?'],
+        input=snippets,
+        stdout=PIPE,
+        text=True
+    )
 
-def complete(text, state):
-    if text == '':
-        matches = snippets
-    else:
-        matches = [snip for snip in snippets if snip.startswith(text)]
+    snippet_name = process.stdout.strip()
+    snippet_path = os.path.join(SNIPPET_DIR, (snippet_name + '.txt'))
 
-    if state > len(matches):
-        single_match = None
-    else:
-        single_match = matches[state]
+    with open(snippet_path, 'r') as f:
+        snippet = f.read()
 
-    return single_match
+    return snippet
+
+def replace_placeholders(snippet: str) -> str:
+    placeholders = re.findall(r'~!~[^(~!~)]*~!~', snippet)
+    finished_placeholders = []
+
+    for placeholder in placeholders:
+        if placeholder not in finished_placeholders:
+            new_text = dmenu_input(placeholder[3: -3] + ': ')
+            snippet = snippet.replace(placeholder, new_text)
+            finished_placeholders.append(placeholder)
+
+    return snippet
+
